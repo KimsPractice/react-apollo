15772b4133962d7eb77c5b0379996b37fa2997b5
kimspractice <tpghksdldia@naver.com>
2020-03-30 11:10:13 +0900

    gql서버에서 쿼리보내 데이터받아보기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,25 @@
-export default () => "Home";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+
+  if (loading) {
+    return <h1>loading</h1>;
+  }
+  if (data && data.movies) {
+    const { movies } = data;
+    return <h1>{movies.map(movie => movie.id)}</h1>;
+  }
+};
